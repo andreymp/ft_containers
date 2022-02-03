51ea116930db2771d512d9090f33c6513cb77666
andreymp <markin.andrey.p@gmail.com>
2022-02-03 21:25:25 +0300

    fixing of vectors

--- Vector.hpp
+++ Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: jobject <jobject@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 21:26:44 by jobject           #+#    #+#             */
-/*   Updated: 2022/02/02 21:24:25 by jobject          ###   ########.fr       */
+/*   Updated: 2022/02/03 21:13:18 by jobject          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,75 +22,91 @@ namespace ft {
     template<class T,class Allocator = std::allocator<T> >
     class Vector {
         public:
-            typedef T                                   value_type;
-            typedef Allocator                           allocator_type;
-            typedef std::size_t                         size_type;
-            typedef std::ptrdiff_t                      difference_type;
-            typedef T&                                  reference;
-            typedef const T&                            const_reference;
-            typedef T*                                  pointer;
-            typedef const T*                            const_pointer;
-            typedef ft::VectorIterator<T>               iterator;
-            typedef ft::ConstVectorIterator<T>          const_iterator;
-            typedef ft::ReverseVectorIterator<T>        reverse_iterator;
-            typedef ft::ConstReverseVectorIterator<T>   const_reverse_iterator;
+            typedef T                                   		value_type;
+            typedef Allocator                           		allocator_type;
+            typedef std::size_t                         		size_type;
+            typedef std::ptrdiff_t                      		difference_type;
+            typedef T&                                 			reference;
+            typedef const T&                            		const_reference;
+            typedef T*                                  		pointer;
+            typedef const T*                            		const_pointer;
+            typedef class ft::VectorIterator<T>			    	iterator;
+            typedef class ft::ConstVectorIterator<T>         	const_iterator;
+            typedef class ft::ReverseVectorIterator<T>       	reverse_iterator;
+            typedef class ft::ConstReverseVectorIterator<T>  	const_reverse_iterator;
         private:
             allocator_type  alloc;
             size_type       _capacity;
             size_type       _size;
 			pointer			ptr;
 			void ft_realloc(size_type new_capacity) {
-				pointer	tmp = alloc.allocate(new_capacity);
-				for (int i = 0 ; i < _size; i++)
-					tmp[i] = ptr[i];
-				alloc.deallocate(ptr, _capacity);
+				pointer tmp = alloc.allocate(new_capacity);
+				for (size_type i = 0; i < _size; i++)
+					alloc.construct(tmp + i, ptr[i]);
+				size_type tmp_size = _size;
+				delVector();
+				_size = tmp_size;
 				_capacity = new_capacity;
 				ptr = tmp;
 			}
-        public:
-            Vector() : alloc(allocator_type()), _capacity(20), _size(0), ptr(nullptr) { ptr = alloc.allocate(20); }
-			Vector(const allocator_type & _alloc) : alloc(_alloc), _capacity(20), _size(0), ptr(nullptr)  { ptr = alloc.allocate(20); }
-			explicit Vector(size_type count, const_reference value = value_type(),
-				const allocator_type & _alloc = allocator_type()) : alloc(_alloc), _capacity(count), _size(0), ptr(nullptr) {
-					ptr = alloc.allocate(count);
-					for (int i = 0; i < _size; i++)
-						ptr[i] = value;
+			void delVector() {
+				if (_capacity > 0) {
+					for (size_type i = 0; i < _size; i++)
+						alloc.destroy(ptr + i);
+					alloc.deallocate(ptr, _capacity);
+					_size = _capacity = 0;
+				}
 			}
-			explicit Vector(size_type count) : alloc(allocator_type()), _capacity(count), _size(0), ptr(nullptr) { ptr = alloc.allocate(count); }
+        public:
+			explicit Vector(const allocator_type & _alloc = allocator_type()) : alloc(_alloc), _capacity(20), _size(0)  { ptr = alloc.allocate(20); }
+			explicit Vector(size_type count, const_reference value = value_type(),
+				const allocator_type & _alloc = allocator_type()) : alloc(_alloc), _capacity(count), _size(count) {
+					ptr = alloc.allocate(count);
+					for (size_type i = 0; i < _size; i++)
+						alloc.construct(ptr + i, value);
+			}
 			Vector(const Vector & other) { *this = other; }
-			~Vector() { alloc.deallocate(ptr, _capacity); }
-			template<class InputIt>
-			Vector(InputIt first, InputIt last, const allocator_type & _alloc = allocator_type()) :
-				alloc(_alloc), _capacity(20), _size(0), ptr(nullptr) {
-				for (InputIt it = first, i = 0; it != last; ++it, ++i, ++_size) {
+			~Vector() { delVector(); }
+			template<typename InputIt>
+			Vector(InputIt first, InputIt last, const allocator_type & _alloc = allocator_type(), typename std::enable_if<!std::numeric_limits<InputIt>::is_specialized>::type * = 0) :
+				alloc(_alloc), _capacity(20), _size(0) {
+				if (first > last)
+					throw std::length_error("vector");
+				int i = 0;
+				ptr = alloc.allocate(_capacity);
+				for (InputIt it = first; it != last; ++it, ++i, ++_size) {
 					if (_size == _capacity)
 						ft_realloc(_capacity * 2);
-					*(ptr + i) = *it;
+					alloc.construct(ptr + i, *it);
 				}
 			}
 			Vector & operator=(const Vector & other) {
 				if (this != &other) {
-					if (ptr)
-						alloc.deallocate(ptr, _capacity);
+					// delVector();
 					alloc = other.alloc;
 					_capacity = other._capacity;
 					_size = other._size;
 					ptr = alloc.allocate(_capacity);
 					for (int i = 0; i < _size; i++)
-						ptr[i] = other[i];
+						alloc.construct(ptr + i, other[i]);
 				}
 				return *this;
 			}
 			void assign(size_type count, const_reference value) {
-				if (ptr)
-					alloc.deallocate(ptr, _capacity);
-				insert(begin(), count, value);
+				delVector();
+				ptr = alloc.allocate(count);
+				_size = _capacity = count;
+				for (int i = 0; i < count; i++)
+					alloc.construct(ptr + i, value);
 			}
 			template<class InputIt>
-			void assign(InputIt first, InputIt last) {
-				if (ptr)
-					alloc.deallocate(ptr, _capacity);
-				insert(begin(), first, last);
+			void assign(InputIt first, InputIt last, typename std::enable_if<!std::numeric_limits<InputIt>::is_specialized>::type * = 0) {
+				delVector();
+				for (InputIt it = first; it != last; ++it, ++_capacity);
+				ptr = alloc.allocate(_capacity);
+				_size = _capacity;
+				for (int i = 0; first != last; ++first, ++i)
+					alloc.construct(ptr + i, *first);
 			}
 			allocator_type get_allocator() const { return alloc; }
 			reference at(size_type pos) {
@@ -132,20 +148,24 @@ namespace ft {
 			void clear() { erase(begin(), end()); }
 			iterator insert(iterator pos, const_reference value) {
 				size_type i = 0;
-				for (iterator it = begin(); it != end() && it != pos; it++, i++);
-				if (_size == _capacity)
+				iterator it;
+				for (it = begin(); it != end() && it != pos; ++it, i++);
+				if (_size + 1 > _capacity)
 					ft_realloc(_capacity * 2);
-				if (i < _size) {
+				if (it != end()) {
 					++_size;
-					for (size_type j = _size - 1; j != i; j--)
-						ptr[j] = ptr[j - 1];
-					ptr[i] = value;
-				} else {
-					while (i >= _capacity)
-						ft_realloc(_capacity * 2);
-					ptr[i] = value;
-					_size = i + 1;
-				}
+					for (size_type j = _size; j != i; j--) {
+						alloc.destroy(ptr + j);
+						alloc.construct(ptr + j, ptr[j - 1]);
+					}
+					alloc.destroy(ptr + i);
+					alloc.construct(ptr + i, value); }
+				// } else {
+				// 	while (i >= _capacity)
+				// 		ft_realloc(_capacity * 2);
+				// 	ptr[i] = value;
+				// 	_size = i + 1;
+				// }
 				return iterator(ptr + i);
 			}
 			void insert(iterator pos, size_type count, const_reference value) {
@@ -153,18 +173,21 @@ namespace ft {
 					pos = insert(pos, value);
 			}
 			template<class InputIt>
-			void insert(iterator pos, InputIt first, InputIt last) {
+			void insert(iterator pos, InputIt first, InputIt last, typename std::enable_if<!std::numeric_limits<InputIt>::is_specialized>::type * = 0) {
 				while (first != last) {
-					pos = insert(pos, &(*first)) + 1;
+					pos = insert(pos, *first) + 1;
 					++first;
 				}
 			}
 			iterator erase(iterator pos) {
 				size_type i = 0;
-				for (iterator it = begin(); it != end() && it != pos; it++, i++);
-				if (i > _size - 1)
-					return end();
-				for (int j = i; j < _size; j++)
+				iterator it;
+				for (it = begin(); it != end() && it != pos; ++it, i++);
+				if (it == end()) {
+					pop_back();
+					return iterator(ptr + _size);
+				}
+				for (int j = i; j < _size - 1; j++)
 					ptr[j] = ptr[j + 1];
 				--_size;
 				return iterator(ptr + i);
@@ -175,23 +198,16 @@ namespace ft {
 				return first;
 			}
 			void push_back(const_reference value) {
-				if (_size == _capacity)
-					ft_realloc(_capacity * 2);
-				ptr[_size++] = value;
+				if (_size + 1 > _capacity)
+					reserve(_capacity ? _capacity * 2 : 20);
+				alloc.construct(ptr + _size, value);
+				++_size;
 			}
 			void pop_back() {
 				if (!empty())
-					erase(end() - 1);
+					--_size;
 			}
-			void resize(size_type count) {
-				if (count < _size)
-					for (size_type tmp = _size; tmp != count; tmp--)
-						pop_back();
-				else
-					for (size_type tmp = _size; tmp != count; tmp++)
-						push_back(0);
-			}
-			void resize(size_type count, value_type value = 0) {
+			void resize(size_type count, value_type value = value_type()) {
 				if (count < _size)
 					for (size_type tmp = _size; tmp != count; tmp--)
 						pop_back();
